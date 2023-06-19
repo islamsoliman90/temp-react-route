d13d78647b0805f01495058b9323bc128b9088bc
islam soliman <islam.soliman1990@gmail.com>
2023-06-19 08:02:56 +0300

    refactor social icons and complete scss files

--- src/compoments/social.js
+++ src/compoments/social.js
@@ -1,32 +1,33 @@
 import React, { Component } from "react";
+import { FaFacebook, FaYoutube, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export default function Social() {
   return (
     <div class="social-media p-20 bg-white rad-10 p-relative">
       <h2 class="mt-0 mb-25">Social Media Stats</h2>
       <div class="box twitter p-15 p-relative mb-10 between-flex">
-        <i class="fa-brands fa-twitter fa-2x c-white h-full center-flex"></i>
+        <FaTwitter />
         <span>90K Followers</span>
         <a class="fs-13 c-white btn-shape" href="#">
           Follow
         </a>
       </div>
       <div class="box facebook p-15 p-relative mb-10 between-flex">
-        <i class="fa-brands fa-facebook-f fa-2x c-white h-full center-flex"></i>
+        <FaFacebook />
         <span>2M Like</span>
         <a class="fs-13 c-white btn-shape" href="#">
           Like
         </a>
       </div>
       <div class="box youtube p-15 p-relative mb-10 between-flex">
-        <i class="fa-brands fa-youtube fa-2x c-white h-full center-flex"></i>
+        <FaYoutube />
         <span>1M Subs</span>
         <a class="fs-13 c-white btn-shape" href="#">
           Subscribe
         </a>
       </div>
       <div class="box linkedin p-15 p-relative mb-10 between-flex">
-        <i class="fa-brands fa-linkedin fa-2x c-white h-full center-flex"></i>
+        <FaLinkedin />
         <span>70K Followers</span>
         <a class="fs-13 c-white btn-shape" href="#">
           Follow
